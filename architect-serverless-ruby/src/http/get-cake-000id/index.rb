2151694ead9a833f81d43b799a06454efd87b9f4
Donald Wu <yeukfei02@gmail.com>
2021-04-22 23:52:42 +0800

    feat: add architect-serverless-ruby db in api

--- architect-serverless-ruby/src/http/get-cake-000id/index.rb
+++ architect-serverless-ruby/src/http/get-cake-000id/index.rb
@@ -1,5 +1,7 @@
 require 'json'
 require 'logger'
+require 'architect/functions'
+require 'aws-sdk'
 
 def handler(event, context)
   logger = Logger.new($stdout)
@@ -7,9 +9,35 @@ def handler(event, context)
   if event['pathParameters']
     id = event['pathParameters']['id']
     logger.info("id = #{id}")
-  end
 
-  {
-    message: 'getCakeById'
-  }
+    data = Arc.reflect
+    table_name = data['tables']['cake']
+
+    client = Aws::DynamoDB::Client.new(
+      region: 'ap-southeast-1',
+    )
+    get_item_response = client.get_item({
+      key: {
+        "id": id
+      }, 
+      table_name: table_name, 
+    })
+    logger.info("get_item_response = #{get_item_response}")
+
+    if get_item_response
+      response = get_item_response.to_h
+      cake = response.item
+      
+      {
+        statusCode: 200,
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: {
+          message: 'getCakeById',
+          cake: cake
+        }.to_json
+      }
+    end
+  end
 end
--- architect-serverless-ruby/src/http/post-cake/index.rb
+++ architect-serverless-ruby/src/http/post-cake/index.rb
@@ -1,5 +1,8 @@
 require 'json'
 require 'logger'
+require 'architect/functions'
+require 'aws-sdk'
+require 'securerandom'
 
 def handler(event, context)
   logger = Logger.new($stdout)
@@ -9,6 +12,24 @@ def handler(event, context)
     price = event['body']['price']
     logger.info("name = #{name}")
     logger.info("price = #{price}")
+
+    data = Arc.reflect
+    table_name = data['tables']['cake']
+
+    client = Aws::DynamoDB::Client.new(
+      region: 'ap-southeast-1',
+    )
+    put_item_response = client.put_item({
+      item: {
+        "id": SecureRandom.uuid,
+        "name": name,
+        "price": price,
+        "createdAt": Time.now.strftime("%Y/%m/%d %H:%M:%S"),
+        "updatedAt": Time.now.strftime("%Y/%m/%d %H:%M:%S"),
+      }, 
+      table_name: table_name,
+    })
+    logger.info("put_item_response = #{put_item_response}")
   end
 
   {
